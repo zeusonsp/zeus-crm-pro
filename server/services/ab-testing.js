7dfeee0ad3a80ca58779dfb768985a181e27afaa
zeusonsp <zeusonsp@gmail.com>
2026-03-29 06:03:14 -0300

    feat(v4.0): add A/B testing service

---
+++ server/services/ab-testing.js
@@ -0,0 +1,7 @@
+/**
+ * Zeus CRM Pro v4.0 - A/B Testing Service
+ * Split campaign audiences and track variant performance
+ */
+const admin = require('firebase-admin');
+const { v4: uuid } = require('uuid');
+
